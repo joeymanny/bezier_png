61b612cda0ea6f6435f3837cb4ed6f7d9c4a8bce
joeymanny <joeymanny1212@gmail.com>
2023-12-14 06:43:28 -0900

    df

--- src/main.rs
+++ src/main.rs
@@ -6,17 +6,12 @@ use rayon::iter::ParallelIterator;
 
 use rand::Rng;
 use rayon::iter::IntoParallelIterator;
-use sdl2::event::{self, Event};
+use sdl2::event::Event;
 
 const MAX_X: u32 = 600;
 const MAX_Y: u32 = 400;
 
-const PROGRESSION: u32 = 1000;
-
-const START: (f32, f32) = (10.0,10.0);
-const END: (f32, f32) = (80.0, 10.0);
-const HANDLE_1: (f32, f32) = (900.0, 50.0);
-const HANDLE_2: (f32, f32) = (70.0, 70.0);
+const PROGRESSION: u32 = 200;
 
 
 
@@ -38,13 +33,17 @@ struct Point{
 }
 
 impl Point{
+    #[allow(unused)]
     const ZERO:Self = Point{x: 0.0, y: 0.0};
-    fn normalized(self) -> Self{
-        let mag = (self.x.powi(2) + self.y.powi(2)).sqrt();
+    fn normalized(&self) -> Self{
+        let mag = self.mag();
         Self {
             x:self.x / mag,
             y: self.y / mag
         }
+    }
+    fn mag(&self) -> f32{
+        (self.x.powi(2) + self.y.powi(2)).sqrt()
     }
 }
 
@@ -70,7 +69,7 @@ fn calc_bezier(v: &[Point], progress: f32) -> Point{
     // (1.0 - progress).powi(2) * p0 + 2.0 * (1.0 - progress) * progress * p1 + progress.powi(2) * p2
     recursive_step(v.len() as u32 - 1, 0, progress, v)
 }
-fn recursive_step(n: u32, mut i: u32, progress: f32, v: &[Point]) -> Point{
+fn recursive_step(n: u32, i: u32, progress: f32, v: &[Point]) -> Point{
     if i == n{
         //  v.get(i as usize).unwrap().clone() * binomial_iter::BinomialIter::new(n, i).binom() as f32 * (1.0 - progress).powi((n - i) as i32) *progress.powi(i as i32)
         // Point{x: 0.0, y: 0.0}
@@ -90,13 +89,13 @@ fn equate(n: u32, i: u32, progress: f32, v: &[Point]) -> Point{
 }
 
 pub fn main() {
-    let writer = new_png_writer("bezier.png", MAX_X, MAX_Y);
-    let mut canvas = PngWriter{
-        writer,
-        buffer: new_buffer_bools(MAX_X, MAX_Y)
-    };
+    // let writer = new_png_writer("bezier.png", MAX_X, MAX_Y);
+    // let canvas = PngWriter{
+    //     writer,
+    //     buffer: new_buffer_bools(MAX_X, MAX_Y)
+    // };
     let mut rand = rand::thread_rng();
-    let mut controls = (0..26).into_iter().map(|_| Point::from((rand.gen::<f32>() * MAX_X as f32, rand.gen::<f32>() * MAX_Y as f32))).collect::<Vec<Point>>();
+    let controls = (0..7).into_iter().map(|_| Point::from((rand.gen::<f32>() * MAX_X as f32, rand.gen::<f32>() * MAX_Y as f32))).collect::<Vec<Point>>();
     let mut curve = Bezier::new(controls);
 
 
@@ -109,7 +108,7 @@ pub fn main() {
         .unwrap();
  
     let mut sdl_canvas = window.into_canvas().accelerated().build().unwrap();
-
+    sdl_canvas.set_blend_mode(sdl2::render::BlendMode::Blend);
     // calculate the points
     let mut points = (0..PROGRESSION).into_par_iter().map(|n|{
         curve.point(n as f32 / PROGRESSION as f32)
@@ -131,55 +130,131 @@ pub fn main() {
 
     // add velocities to points
     let mut event_pump = sdl_context.event_pump().unwrap();
+    let mut should_move = true;
     'running: loop{
+        let timer = std::time::Instant::now();
+        sdl_canvas.set_draw_color(sdl2::pixels::Color::RGB(0, 0, 0,));
+        sdl_canvas.clear();
+        sdl_canvas.set_draw_color(sdl2::pixels::Color::RGB(255, 255, 255));
         for event in event_pump.poll_iter(){
             match event{
                 Event::Quit { .. } => break 'running,
+                Event::KeyDown { keycode: Some(sdl2::keyboard::Keycode::Up), ..} => {
+                    if curve.points.len() < 35{
+                        curve.points.push(Point{x: rand.gen::<f32>() * MAX_X as f32, y: rand.gen::<f32>() * MAX_Y as f32});
+                    }
+                },
+                Event::KeyDown { keycode: Some(sdl2::keyboard::Keycode::Down), .. } => {
+                    if curve.points.len() > 2{
+                        curve.points.pop();
+                    }
+                },
+                Event::KeyDown { keycode: Some(sdl2::keyboard::Keycode::Space), .. } => {
+                    should_move = !should_move
+                }
                 _ => ()
              }
         }
-        if event_pump.keyboard_state().is_scancode_pressed(sdl2::keyboard::Scancode::LShift){
-            let controls = (0..7).into_iter().map(|_| Point::from((rand.gen::<f32>() * MAX_X as f32, rand.gen::<f32>() * MAX_Y as f32))).collect::<Vec<Point>>();
-            curve = Bezier::new(controls);
 
-        }
-        if event_pump.keyboard_state().is_scancode_pressed(sdl2::keyboard::Scancode::Space){
-            velocities = (0..points.len()).map(|_|{
-                Point{
-                    x: rand.gen::<f32>() * 0.5 - 0.25,
-                    y: rand.gen::<f32>() * 0.5 - 0.25
+        {
+            let m = event_pump.mouse_state();
+            if m.is_mouse_button_pressed(sdl2::mouse::MouseButton::Left){
+                let x = m.x();
+                let y = m.y();
+                let mut closest = f32::MAX;
+                let mut index = 0;
+                for (i, point) in curve.points.iter().enumerate(){
+                    let dist = ((point.x - x as f32).powi(2) + (point.y - y as f32).powi(2)).sqrt();
+                    if dist.abs() < closest{
+                        closest = dist;
+                        index = i;
+                    }
                 }
-            }).collect::<Vec<Point>>();
-        }
+                curve.points[index] = Point{x: x as f32, y: y as f32}
 
+            }
+        }
+        // if event_pump.keyboard_state().is_scancode_pressed(sdl2::keyboard::Scancode::LShift){
+        //     let controls = (0..3).into_iter().map(|_| Point::from((rand.gen::<f32>() * MAX_X as f32, rand.gen::<f32>() * MAX_Y as f32))).collect::<Vec<Point>>();
+        //     curve = Bezier::new(controls);
+
+        // }
+        // if event_pump.keyboard_state().is_scancode_pressed(sdl2::keyboard::Scancode::Space){
+        //     velocities = (0..points.len()).map(|_|{
+        //         Point{
+        //             x: rand.gen::<f32>() * 0.5 - 0.25,
+        //             y: rand.gen::<f32>() * 0.5 - 0.25
+        //         }
+        //     }).collect::<Vec<Point>>();
+        // }
+        {
+            // let k = event_pump.keyboard_state();
+            // if k.is_scancode_pressed(sdl2::keyboard::Scancode::Up){
+            //     curve.points.push(Point{x: rand.gen::<f32>() * MAX_X as f32, y: rand.gen::<f32>() * MAX_Y as f32});
+            // }
+            // if k.is_scancode_pressed(sdl2::keyboard::Scancode::Down) && curve.points.len() > 2{
+            //     curve.points.pop();
+            // }
+        }
         points = (0..PROGRESSION).into_par_iter().map(|n|{
             curve.point(n as f32 / PROGRESSION as f32)
         }).collect::<Vec<Point>>();
+        if should_move{
 
-        curve.points = curve.points.into_iter().zip(velocities.clone()).map(|(p, v)|{
-            let x = (p.x + v.x + MAX_X as f32) % MAX_X as f32;
-            let y = (p.y + v.y + MAX_Y as f32) % MAX_Y as f32;
-            Point{
-                x,
-                y
-            }
-        }).collect();
-
+        
+            curve.points = curve.points.into_iter().zip(velocities.iter_mut()).map(|(p, v)|{
+                let x = p.x + v.x;
+                let y = p.y + v.y;
+                if x > MAX_X as f32 || x < 0.0{
+                    v.x = -v.x
+                }
+                if y > MAX_Y as f32 || y < 0.0{
+                    v.y = -v.y
+                }
+                Point{
+                    x,
+                    y
+                }
+            }).collect::<Vec<Point>>();
+        }
         
 
 
         let sdl_points = points.clone().into_iter().map(|p|sdl2::rect::Point::from(p)).collect::<Vec<sdl2::rect::Point>>();
 
-        sdl_canvas.set_draw_color(sdl2::pixels::Color::RGB(0, 0, 0,));
-        sdl_canvas.clear();
-        sdl_canvas.set_draw_color(sdl2::pixels::Color::RGB(255, 255, 255));
-        let _ = sdl_canvas.draw_points(&*sdl_points);
+        let _ = sdl_canvas.draw_lines(&*sdl_points);
         sdl_canvas.set_draw_color(sdl2::pixels::Color::RGB(0, 0, 255,));
+        for (i, point) in curve.points.iter().enumerate(){
+            {
+                let max = curve.points.len() as f32;
+                sdl_canvas.set_draw_color(sdl2::pixels::Color::RGBA(
+                        (((max - i as f32) / max) * 255.0) as u8,   // r
+                        0,                                                // g
+                        ((i as f32 / max) * 255.0) as u8,                   // b
+                        100
+                    )
+                )
+            }
+            let un = sdl2::rect::Point::new(point.x as i32 - 5, point.y as i32 - 5);
+            let four = sdl2::rect::Point::new(point.x as i32 + 5, point.y as i32 + 5);
+            let doia = sdl2::rect::Point::new(point.x as i32 - 5, point.y as i32 + 5);
+            let toia = sdl2::rect::Point::new(point.x as i32 + 5, point.y as i32 - 5);
+            
+            let _ = sdl_canvas.draw_line(un, four);
+            let _ = sdl_canvas.draw_line(doia, toia);
+        }
+        let sdl_points = curve.points.clone().into_iter().map(|p|sdl2::rect::Point::from(p)).collect::<Vec<sdl2::rect::Point>>();
+        sdl_canvas.set_draw_color(sdl2::pixels::Color::RGBA(0, 255, 255, 25));
+        let _ = sdl_canvas.draw_lines(&*sdl_points);
+
         // sdl_canvas.draw_points(curve.points.iter().map(
             // |v| sdl2::rect::Point::from(v.clone())
         // ).collect());
         sdl_canvas.present();
-        // std::thread::sleep(std::time::Duration::from_secs_f32(0.1));
+        // std::thread::sleep(std::time::Duration::from_secs_f32(0.01));
+        std::thread::sleep(std::time::Duration::from_millis(10).saturating_sub(timer.elapsed()));
+        // let remaining = timer.elapsed().saturating_sub(std::time::Duration::from_secs_f32(0.01));
+        // std::time::Duration::from_secs_f32(0.01).saturating_sub(timer.elapsed());
         // canvas.write().unwrap();
     }
 }
@@ -224,7 +299,8 @@ impl core::ops::Add for Point{
 impl PngWriter{
     // the draw_triangle function used by the Boid modifies the 
     // buffer, this function will submit the buffer to a file
-    fn write(&mut self) -> Result<(), png::EncodingError>{
+#[allow(unused)]
+fn write(&mut self) -> Result<(), png::EncodingError>{
         self.writer.write_image_data(
             // this function take a Vec<u8>. With a bit depth of
             // one, this is 8 pixels per u8. the width/height won't
@@ -253,6 +329,7 @@ impl PngWriter{
 
 // buffer of bools we can 'draw' to
 // false represents a dark pixel, true represents a light pixel
+#[allow(unused)]
 fn new_buffer_bools(width: u32, height: u32) -> Vec<Vec<bool>>{
     // for every row
     (0..height).into_iter()
@@ -270,6 +347,7 @@ fn new_buffer_bools(width: u32, height: u32) -> Vec<Vec<bool>>{
 // ex.
 // [   t, t, f, f, t, t, f, f,     t, t,]
 // [0b_1__1__0__0__1__1__0__0_, 0b_1__1__000000] <- which is [204, 192]
+#[allow(unused)]
 fn vec_u8_from_vec_bool(v: Vec<bool>) -> Vec<u8>{
     let mut out: Vec<u8> = vec![];
     // we go over the array of bools 8 at a time
@@ -302,6 +380,7 @@ fn vec_u8_from_vec_bool(v: Vec<bool>) -> Vec<u8>{
     out
 }
 
+#[allow(unused)]
 fn new_png_writer(path: &str,width: u32, height: u32) -> png::Writer<BufWriter<File>>{
     // open a new file or overwrite an existing one with truncate
     let path = std::path::Path::new(path);
@@ -327,12 +406,15 @@ fn new_png_writer(path: &str,width: u32, height: u32) -> png::Writer<BufWriter<F
 }
 
 
+#[allow(unused)]
 fn draw_point<T: Into<(u32, u32)>>(data: &mut Vec<Vec<bool>>, point: T) -> Result<(), ()>{
     edit_point(data, point.into(), true)
 }
+#[allow(unused)]
 fn erase_point<T: Into<(u32, u32)>>(data: &mut Vec<Vec<bool>>, point: T) -> Result<(), ()>{
     edit_point(data, point.into(), false)
 }
+#[allow(unused)]
 fn edit_point(data: &mut Vec<Vec<bool>>, point: (u32, u32), what: bool) -> Result<(), ()>{
     if let Some(x) = data.get_mut(point.1 as usize){
         if let Some(y) = x.get_mut(point.0 as usize){
